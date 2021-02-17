33a4551ea37c369f7bad8cf9e4246ca2ccb493d5
sarahsookoo <sarah_sookoo@outlook.com>
2021-02-17 11:32:33 -0500

    index.html

---
+++ funfacts.js
@@ -0,0 +1,9 @@
+function facts(){
+    var facts = [
+        "5:11pm 2/16/2021 Tanmoy and I came up with the idea of this website",
+        "5:22pm Fushia was chosen by Tanmoy as a theme color",
+        "5:23 Burgundy was chosen by Sharmin as a theme color"
+    ]
+    for (i = 0; i < facts.length; i++)
+        document.writeln((i+1) + ": " + array[i]);
+}

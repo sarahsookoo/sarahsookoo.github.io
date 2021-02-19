9934d2eb52ac572c02030422f5f6dc0fe12e24e7
sarahsookoo <sarah_sookoo@outlook.com>
2021-02-19 12:32:16 -0500

    html file

--- funfacts.js
+++ funfacts.js
@@ -1,9 +1,10 @@
 function facts(){
     //  var myWindow = window.open("", "_blank");
-    var myWindow = window.open("", "_blank", "fullscreen=yes");
-    myWindow.document.write("<p>5:11pm 2/16/2021 Tanmoy and I came up with the idea for this website</p>");
+    // var myWindow = window.open("facts.html", "_blank", "fullscreen=yes");
+    var myWindow = window.open("facts.html", "_blank");
+    // myWindow.document.write("<p>5:11pm 2/16/2021 Tanmoy and I came up with the idea for this website</p>");
     
-    myWindow.document.write("<p>5:22pm 2/16/2021 Fuchsia was chosen by Tanmoy as a theme color</p>");
+    // myWindow.document.write("<p>5:22pm 2/16/2021 Fuchsia was chosen by Tanmoy as a theme color</p>");
     
-    myWindow.document.write("<p>5:23 2/16/2021 Burgundy was chosen by Sharmin as a theme color</p>");
+    // myWindow.document.write("<p>5:23pm 2/16/2021 Burgundy was chosen by Sharmin as a theme color</p>");
 }

9b6be740532dde8f57a8ccd16732bdc80ce7daad
sarahsookoo <sarah_sookoo@outlook.com>
2021-03-01 16:15:55 -0500

    added buttons

---
+++ freshman/buttons.js
@@ -0,0 +1,11 @@
+function freshman1(){
+    var myWindow = window.open("freshman1.html", "_blank");
+ }
+
+function winter1(){
+    var myWindow = window.open("winter1.html", "_blank");
+ }
+
+ function freshman2(){
+    var myWindow = window.open("freshman2.html", "_blank");
+ }

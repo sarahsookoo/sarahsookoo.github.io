17f956a6f94981eaea72fc5ccd8eeaefe60e1d92
sarahsookoo <sarah_sookoo@outlook.com>
2021-03-06 16:47:12 -0500

    formatting

--- freshman/buttons.js
+++ freshman/buttons.js
@@ -1,11 +1,11 @@
 function freshman1(){
-    var myWindow = window.open("freshman1.html", "_blank");
+    var myWindow = window.open("freshman1.html");
  }
 
 function winter1(){
-    var myWindow = window.open("winter1.html", "_blank");
+    var myWindow = window.open("winter1.html");
  }
 
  function freshman2(){
-    var myWindow = window.open("freshman2.html", "_blank");
+    var myWindow = window.open("freshman2.html");
  }

6fb7e0c075a6cce2f40f6c32a9803cf3117701bf
sarahsookoo <sarah_sookoo@outlook.com>
2021-03-06 18:03:12 -0500

    clock

--- freshman/buttons.js
+++ freshman/buttons.js
@@ -1,11 +1,11 @@
 function freshman1(){
-    var myWindow = window.open("freshman1.html");
+    var myWindow = window.open("freshman1.html", target="_self");
  }
 
 function winter1(){
-    var myWindow = window.open("winter1.html");
+    var myWindow = window.open("winter1.html", target="_self");
  }
 
  function freshman2(){
-    var myWindow = window.open("freshman2.html");
+    var myWindow = window.open("freshman2.html", target="_self");
  }

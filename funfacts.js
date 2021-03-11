1cb008dca29a3096549d61182eb4e90e6f5ba0ac
sarahsookoo <sarah_sookoo@outlook.com>
2021-03-10 21:22:24 -0500

    trivia3

--- funfacts.js
+++ funfacts.js
@@ -1,8 +1,9 @@
 var questions = [" How many soccer players should each team have on the field at the start of each match?",
 "What year was the very first model of the iPhone released?",
-"What does HTTP stand for?"]
+"What does HTTP stand for?", "How many legs does a spider have?", "How many days are in a year?",
+"What is a group of lions called?", "What is the name of Harry Potters pet owl?"]
 
-var answers = ["11", "2007", "HyperText Transfer Protocol"];
+var answers = ["11", "2007", "HyperText Transfer Protocol", "8", "365", "A pride","Hedwig"];
 
 var num = Math.floor(Math.random()*questions.length);
 
@@ -15,7 +16,6 @@ function trivia(){
 }
 
 function problems(){
-   
 var randomQues = questions[num];
 //document.writeln(randomQues);
 document.getElementById("question").innerHTML = randomQues;
@@ -25,9 +25,11 @@ function checkAns(){
     var input = document.getElementsByName("ans")[0].value;
     if (input == answers[num]){
         document.getElementById("ans").innerHTML = "Congrats, you're correct";
+        unHide3();
     }
     else {
-        document.getElementById("ans").innerHTML = "Wrong Answer";
+        document.getElementById("ans").innerHTML = "Wrong Answer, the correct answer is " + answers[num];
+        //unHide3();
     }
 }
 
@@ -55,6 +57,38 @@ function unHide2(){
     if(z.style.visibility = "hidden"){
         z.style.visibility = "visible";
     }
+}
+
+function unHide3(){
+    var z = document.getElementsByClassName("show3")[0];
+    if(z.style.visibility = "hidden"){
+        z.style.visibility = "visible";
+    }
+}
+
+// function hide(){
+//     var z = document.getElementsByClassName("show2")[0];
+//     if(z.style.visibility = "visible"){
+//         z.style.visibility = "hidden";
+//     }
+// }
+
+function replace(){
+    document.getElementById('show2').innerHTML = '';
+
+    var newQues = document.createElement('p');
+    newQues.innerHTML = questions[Math.floor(Math.random()*questions.length)];
+    document.getElementById('show2').appendChild(newQues);
+
+    var i = document.createElement("input"); //input element, text
+    i.setAttribute('type',"text");
+    document.getElementById('show2').appendChild(i);
+
+    var b = document.createElement("button"); //input element, text
+    b.setAttribute('type',"button");
+    b.setAttribute('value',"submit");
+    // b.setAttribute('onclick', checkAns())
+    document.getElementById('show2').appendChild(b);
 }
 
   

7905ee9efcdc82826bbf71a3bfcfbf08f16260c4
sarahsookoo <sarah_sookoo@outlook.com>
2021-04-04 21:38:18 -0400

    css styles

--- art/art.js
+++ art/art.js
@@ -7,6 +7,8 @@ function checkName(){
         document.getElementById("name").style.visibility = "hidden";
         document.getElementById("button").style.visibility = "hidden";
         document.getElementById("hello").style.visibility = "visible";
+        document.getElementById("hello2").style.visibility = "visible";
         document.getElementById("demo").innerHTML = x;
+        document.getElementById("demo2").innerHTML = x;
     }
 }

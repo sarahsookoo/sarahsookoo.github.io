b2d1adb836755135fe3fea0e5317acff8688c272
sarahsookoo <sarah_sookoo@outlook.com>
2021-07-15 13:17:59 -0400

    sophomore format 4

---
+++ sophomore/buttons.js
@@ -0,0 +1,15 @@
+function sophomore1(){
+    var myWindow = window.open("sophomore.html", target="_self");
+ }
+
+function winter2(){
+    var myWindow = window.open("winter2.html", target="_self");
+ }
+
+ function sophomore2(){
+    var myWindow = window.open("sophomore2.html", target="_self");
+ }
+
+ function home(){
+   var myWindow = window.open("https://sarahsookoo.github.io./", target="_self");
+}

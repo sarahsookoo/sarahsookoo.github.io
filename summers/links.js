eac73423d1776fb53537bcae388348da21adf735
sarahsookoo <sarah_sookoo@outlook.com>
2021-07-20 15:29:59 -0400

    linkanimate4

--- summers/links.js
+++ summers/links.js
@@ -1,4 +1,30 @@
 
-    //     function func () {
-    //      alert("Hello")
-    //  }
+function func1(){
+    document.getElementById("one").style.animation = "move 1s 1";
+}
+
+function func2(){
+    document.getElementById("two").style.animation = "move 1s 1";
+}
+
+
+function func3(){
+    document.getElementById("three").style.animation = "move 1s 1";
+}
+
+
+function func4(){
+    document.getElementById("four").style.animation = "move 1s 1";
+}
+
+function func5(){
+    document.getElementById("five").style.animation = "move 1s 1";
+}
+
+function func6(){
+    document.getElementById("six").style.animation = "move 1s 1";
+}
+
+function func7(){
+    document.getElementById("seven").style.animation = "move 1s 1";
+}
